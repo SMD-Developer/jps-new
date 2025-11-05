343c327dcacaa4f4a9f902e01cbc09a4245f1eeb
ashutosh8543 <ashutosh@smdwebtech.com>
2025-11-05 16:46:37 +0530

    payment notes

--- core/resources/views/clientarea/payments/success.blade.php
+++ core/resources/views/clientarea/payments/success.blade.php
@@ -187,10 +187,11 @@
             @lang('app.dashboard')
         </a>
       @endif
-
-      <p class="mt-3 text-muted fw-semibold" style="font-size: 14px;">
-          <strong>Nota:</strong> Sila klik ‘Lihat Resit’ untuk muat turun dan cetak resit untuk tujuan rekod.
-      </p>
+      @if($transactionStatus == 'SUCCESSFUL')
+        <p class="mt-3 text-muted fw-semibold" style="font-size: 14px;">
+            <strong>Nota:</strong> Sila klik ‘Lihat Resit’ untuk muat turun dan cetak resit untuk tujuan rekod.
+        </p>
+      @endif
     </div>
     
   </div>
