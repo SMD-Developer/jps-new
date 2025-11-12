9d7370aa90e41ba83382d5b39cb5a6524c92efad
ashutosh8543 <ashutosh@smdwebtech.com>
2025-11-12 12:57:53 +0530

    third party payment

--- core/resources/views/third-party/payments/b2c-checkout.blade.php
+++ core/resources/views/third-party/payments/b2c-checkout.blade.php
@@ -28,40 +28,62 @@
                 </div>
 
                 <!-- FPX Payment Form -->
-                <form name="form1" method="post" action="{{ $actionUrl }}">
-                    <!-- 🔥 ALL REQUIRED FPX PARAMETERS -->
-                    <input type="hidden" name="fpx_msgType" value="{{ $fpx_msgType }}">
-                    <input type="hidden" name="fpx_msgToken" value="{{ $fpx_msgToken }}">
-                    <input type="hidden" name="fpx_sellerExId" value="{{ $fpx_sellerExId }}">
-                    <input type="hidden" name="fpx_sellerExOrderNo" value="{{ $fpx_sellerExOrderNo }}">
-                    <input type="hidden" name="fpx_sellerTxnTime" value="{{ $fpx_sellerTxnTime }}">
-                    <input type="hidden" name="fpx_sellerOrderNo" value="{{ $fpx_sellerOrderNo }}">
-                    <input type="hidden" name="fpx_sellerId" value="{{ $fpx_sellerId }}">
-                    <input type="hidden" name="fpx_sellerBankCode" value="{{ $fpx_sellerBankCode }}">
-                    <input type="hidden" name="fpx_txnCurrency" value="{{ $fpx_txnCurrency }}">
-                    <input type="hidden" name="fpx_txnAmount" value="{{ $fpx_txnAmount }}">
-                    <input type="hidden" name="fpx_buyerEmail" value="{{ $fpx_buyerEmail }}">
-                    <input type="hidden" name="fpx_checkSum" value="{{ $fpx_checkSum }}">
-                    <input type="hidden" name="fpx_buyerName" value="{{ $fpx_buyerName }}">
-                    <input type="hidden" name="fpx_buyerBankId" value="{{ $fpx_buyerBankId }}">
-                    <input type="hidden" name="fpx_buyerBankBranch" value="{{ $fpx_buyerBankBranch }}">
-                    <input type="hidden" name="fpx_buyerAccNo" value="{{ $fpx_buyerAccNo }}">
-                    <input type="hidden" name="fpx_buyerId" value="{{ $fpx_buyerId }}">
-                    <input type="hidden" name="fpx_makerName" value="{{ $fpx_makerName }}">
-                    <input type="hidden" name="fpx_buyerIban" value="{{ $fpx_buyerIban }}">
-                    <input type="hidden" name="fpx_version" value="{{ $fpx_version }}">
-                    <input type="hidden" name="fpx_productDesc" value="{{ $fpx_productDesc }}">
-                    
-                    <!-- 🔥 CALLBACK URLs - CRITICAL! -->
-                    <input type="hidden" name="fpx_callbackUrl" value="{{ $fpx_callbackUrl }}">
-                    <input type="hidden" name="fpx_returnUrl" value="{{ $fpx_returnUrl }}">
+               <form name="form1" method="post" action="{{ $actionUrl }}">
+                    <!-- All Hidden FPX Parameters -->
+                    <input type="hidden" value="{{ $fpx_msgType }}" name="fpx_msgType">
+                    <input type="hidden" value="{{ $fpx_msgToken }}" name="fpx_msgToken">
+                    <input type="hidden" value="{{ $fpx_sellerExId }}" name="fpx_sellerExId">
+                    <input type="hidden" value="{{ $fpx_sellerExOrderNo }}" name="fpx_sellerExOrderNo">
+                    <input type="hidden" value="{{ $fpx_sellerTxnTime }}" name="fpx_sellerTxnTime">
+                    <input type="hidden" value="{{ $fpx_sellerOrderNo }}" name="fpx_sellerOrderNo">
+                    <input type="hidden" value="{{ $fpx_sellerId }}" name="fpx_sellerId">
+                    <input type="hidden" value="{{ $fpx_sellerBankCode }}" name="fpx_sellerBankCode">
+                    <input type="hidden" value="{{ $fpx_txnCurrency }}" name="fpx_txnCurrency">
+                    <input type="hidden" value="{{ $fpx_txnAmount }}" name="fpx_txnAmount">
+                    <input type="hidden" value="{{ $fpx_buyerEmail }}" name="fpx_buyerEmail">
+                    <input type="hidden" value="{{ $fpx_checkSum }}" name="fpx_checkSum">
+                    <input type="hidden" value="{{ $fpx_buyerName }}" name="fpx_buyerName">
+                    <input type="hidden" value="{{ $fpx_buyerBankId }}" name="fpx_buyerBankId">
+                    <input type="hidden" value="{{ $fpx_buyerBankBranch }}" name="fpx_buyerBankBranch">
+                    <input type="hidden" value="{{ $fpx_buyerAccNo }}" name="fpx_buyerAccNo">
+                    <input type="hidden" value="{{ $fpx_buyerId }}" name="fpx_buyerId">
+                    <input type="hidden" value="{{ $fpx_makerName }}" name="fpx_makerName">
+                    <input type="hidden" value="{{ $fpx_buyerIban }}" name="fpx_buyerIban">
+                    <input type="hidden" value="{{ $fpx_version }}" name="fpx_version">
+                    <input type="hidden" value="{{ $fpx_productDesc }}" name="fpx_productDesc">
+                                
+                                <!-- Test Case Information (if applicable) -->
+                                @if(isset($testCase))
+                                    <div class="alert alert-info mb-4">
+                                        <h6 class="alert-heading">Test Mode</h6>
+                                        <p class="mb-1"><strong>Test Case:</strong> {{ $testCase }}</p>
+                                        <p class="mb-0"><strong>Bank:</strong> {{ $fpx_buyerBankBranch }} ({{ $fpx_buyerBankId }})</p>
+                                    </div>
+                                @endif
 
-                    <div class="text-center my-4">
-                        <button type="submit" class="btn btn-primary btn-lg px-5 py-3 shadow-sm">
-                            <i class="fa fa-lock me-2"></i> Proceed to FPX Payment
-                        </button>
-                    </div>
-                </form>
+                                <!-- Payment Button -->
+                                <div class="text-center mb-4">
+                                    <button type="submit" class="btn btn-primary btn-lg px-5 py-3">
+                                        <i class="fa fa-lock mr-2"></i>Proceed to FPX Payment
+                                    </button>
+                                </div>
+
+                                <!-- FPX Logo -->
+                                <div class="text-center mb-4">
+                                    <img src="{{ asset('assets/images/Logo-FPX.png') }}" alt="FPX Payment" class="img-fluid" style="max-width: 100px;">
+                                </div>
+
+                                <!-- Important Instructions -->
+                                <div class="alert alert-warning">
+                                    <h6 class="alert-heading">Important Instructions</h6>
+                                    <ul class="mb-0 pl-3">
+                                        <li>You must have an Internet Banking account to make transactions using FPX</li>
+                                        <li>Please disable your browser's pop-up blocker to avoid interruptions during the transaction</li>
+                                        <li>Do not close the browser or refresh the page until you receive a response</li>
+                                        <li>This transaction is secure and encrypted</li>
+                                    </ul>
+                                </div>
+                            </form>
 
                 <!-- FPX Logo -->
                 <div class="text-center mb-4">
