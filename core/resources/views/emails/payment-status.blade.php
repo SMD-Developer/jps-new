cfb8aac103e243e967e3c5290b8b8cacc968e649
ashutosh8543 <ashutosh@smdwebtech.com>
2026-01-05 11:17:11 +0530

    payment status email

--- core/resources/views/emails/payment-status.blade.php
+++ core/resources/views/emails/payment-status.blade.php
@@ -34,7 +34,7 @@
 
         .title {
             text-align: left;
-            color: #ff6600;
+            color: #000;
             font-size: 18px;
             font-weight: bold;
             padding: 10px 20px;
@@ -78,41 +78,26 @@
         </tr>
         <tr>
             <td class="title">
-                Permohonan Diterima Untuk Semakan
+                Terima kasih kerana menggunakan <strong>Portal e-CP</strong>.
+                Sukacita dimaklumkan bahawa pembayaran anda telah diterima
             </td>
         </tr>
         <tr>
             <td class="content">
-                <p>Pelanggan yang dihormati,</p>
-                <p><strong>Pambayaran anda telah diterima</strong></p>
+                <p><strong>Butiran Transaksi:</strong></p>
                 <p><strong>Nombor Resit: {{ $application->reciept_number }}</strong></p>
                 <p><strong>Rujukan Transaksi: {{ $application->transaction }}</strong></p>
                 <p><strong>Status Pembayaran: <span style="color: #00AA00;">{{ $application->payment_status }}</span></strong></p>
                 
-                <p>Permohonan anda kini sedang diproses ke peringkat seterusnya. Anda boleh menyemak status permohonan anda pada bila-bila masa dengan log masuk ke akaun anda.</p>
-                
-                <p style="margin-top: 20px;">
-                    <a href="https://ecp-jps.selangor.gov.my/clientarea/login" style="background-color: #4CAF50; color: white; padding: 10px 15px; text-decoration: none; border-radius: 4px;">Lihat Permohonan</a>
-                </p>
-                
-                <p style="margin-top: 20px;">Yang benar,</p>
+                <p>Anda boleh mencetak resit bayaran dengan log masuk ke akaun anda</p>
+                <p>Permohonan – Sejarah Caruman</p>
+                <p>👉 <a href="https://ecp-jps.selangor.gov.my/clientarea/login" style="color: blue;">Klik di sini untuk log masuk.</a></p>
             </td>
         </tr>
         <tr>
             <td class="footer">
+                <p style="margin-top: 10px;">Yang benar,</p>
                 <p>Portal e-CP Caruman Parit</p>
-            </td>
-        </tr>
-        <tr>
-            <td class="footer-container">
-                <h3>JOM BERHUBUNG                </h3>
-                <p>Jika anda mempunyai sebarang soalan, lawati tapak sokongan kami di <a href="https://www.jps.com">https://www.jps.com</a>,<br>
-                    hubungi kami di <a href="mailto:support@jps.com">support@jps.com</a>
-                </p>
-                <p>E-mel ini adalah sulit. Ia juga mungkin mendapat keistimewaan dari segi undang-undang. Jika anda bukan penerima, anda tidak boleh menyalin, memajukan, mendedahkan atau menggunakan mana-mana bahagian daripadanya. Jika anda tersilap menerima mesej ini, sila padamkannya dan maklumkan kepada penghantar dengan segera melalui e-mel balasan. Komunikasi Internet tidak boleh dijamin tepat pada masanya, selamat, bebas ralat atau bebas virus. Pengirim tidak menerima liabiliti untuk sebarang kesilapan atau ketinggalan.
-                </p>
-                <p>"JIMAT KERTAS - FIKIRKAN SEBELUM ANDA CETAK"</p>
-                <p>© Hak Cipta 2025. Hak Cipta Terpelihara</p>
             </td>
         </tr>
     </table>
