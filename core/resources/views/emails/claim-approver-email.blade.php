872f8a8bb89785db114f6d9e8c340b7da453fb03
ashutosh8543 <ashutosh@smdwebtech.com>
2026-01-08 10:50:04 +0530

    email logo modification

--- core/resources/views/emails/claim-approver-email.blade.php
+++ core/resources/views/emails/claim-approver-email.blade.php
@@ -83,7 +83,7 @@
     <table>
         <tr>
             <td class="header">
-                <img src="{{ asset('assets/images/selangor.png') }}" alt="Company Logo" class="logo" width="30%">
+                <img src="{{ asset('assets/images/uploads/settings/1765011938.png') }}" alt="Company Logo" class="logo" width="30%">
             </td>
         </tr>
         <tr>
--- core/resources/views/emails/password-reset.blade.php
+++ core/resources/views/emails/password-reset.blade.php
@@ -12,7 +12,7 @@
 <body>
     <div class="container">
         <div class="header">
-            <img src="{{ asset('assets/images/selangor.png') }}" alt="Company Logo" class="logo" width="30%">
+            <img src="{{ asset('assets/images/uploads/settings/1765011938.png') }}" alt="Company Logo" class="logo" width="30%">
             <div class="title">Tetapkan Semula Kata Laluan Anda</div>
         </div>
         <div class="content">
