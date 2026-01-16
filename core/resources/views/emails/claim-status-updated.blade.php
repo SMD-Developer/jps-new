6002c8147209a0c67e96fe38be38f5dcc5d79f85
ashutosh8543 <ashutosh@smdwebtech.com>
2026-01-16 15:15:39 +0530

    status updated

--- core/resources/views/emails/claim-status-updated.blade.php
+++ core/resources/views/emails/claim-status-updated.blade.php
@@ -116,6 +116,12 @@
             background-color: #e0e0e0;
             margin: 15px 0;
         }
+        .footer-title{
+            color:#000 !important;
+        }
+        .bottom-title{
+            color:#000 !important;
+        }
     </style>
 </head>
 
@@ -133,7 +139,7 @@
         </tr>
         <tr>
             <td class="content">
-                <p>Assalamualaikum / Salam Sejahtera Pengguna yang Dihargai, {{ $notifiable->name }}</p>
+                <p>Assalamualaikum / Salam Sejahtera Pengguna yang Dihargai,{{ $notifiable->name }}</p>
                 <p>Dimaklumkan bahawa status tuntutan Caruman Parit anda telah dikemas kini.</p>
 
                 <div class="claim-details">
@@ -157,11 +163,13 @@
                 </div>
             </td>
         </tr>
+        <tr>
+            <td>
+                <p class="footer-title">Terima kasih</p>
+                <p class="bottom-title"><strong>Portal e-CP Caruman Parit</strong></p>
+            </td>
+        </tr>
     </table>
-    <div>
-        <p>Terima kasih</p>
-        <p><strong>Portal e-CP Caruman Parit</strong></p>
-    </div>
 </body>
 
 </html>
