8721c361339eef647ab81039c1d090fe6f82fffc
ashutosh8543 <ashutosh@smdwebtech.com>
2026-01-21 14:30:33 +0530

    approver applications

--- core/resources/views/emails/approverApplication.blade.php
+++ core/resources/views/emails/approverApplication.blade.php
@@ -50,7 +50,7 @@
             text-align: left;
             padding: 20px;
             font-size: 12px;
-            color: #777;
+            color: #000;
         }
 
         .footer-container {
