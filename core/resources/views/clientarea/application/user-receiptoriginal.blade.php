dc29be9f84444dbe489ba981f1b8dbc40cf87402
ashutosh8543 <ashutosh@smdwebtech.com>
2026-01-23 16:32:30 +0530

    user receipt original

--- core/resources/views/clientarea/application/user-receiptoriginal.blade.php
+++ core/resources/views/clientarea/application/user-receiptoriginal.blade.php
@@ -112,6 +112,7 @@
                                             $methodLabel = match($application->payment_method) {
                                                 'FPX_B2C' => 'EFT_B2C',
                                                 'FPX_B2B' => 'EFT_B2B',
+                                                'bank_draf'=> 'BANK DRAF',
                                                 default => $application->payment_method,
                                             };
                                         @endphp
