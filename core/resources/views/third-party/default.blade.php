eb32bead77ae34b46825c25847b760c9f226cd78
ashutosh8543 <ashutosh@smdwebtech.com>
2026-01-29 15:15:25 +0530

    third party

--- core/resources/views/third-party/default.blade.php
+++ core/resources/views/third-party/default.blade.php
@@ -357,7 +357,7 @@ input.btn.btn-primary.login-button.btn-sm.form-control {
     background: #3649B8;
 }
 body {
-    background-image: url("https://ecp-jps.selangor.gov.my/assets/images/JPS logo.jpg");
+    background-image: url("https://ecp-jps.selangor.gov.my/assets/images/background eCP-thirdPary.jpg");
     background-size: cover; 
     background-position: center; /* Centers the image */
     background-repeat: no-repeat; /* Prevents the image from repeating */
