b3bb46f7493a3f9089faa35c99bdd30468818b4f
ashutosh8543 <ashutosh@smdwebtech.com>
2026-01-29 12:21:34 +0530

    third party reset password

--- core/resources/views/third-party/reset-password.blade.php
+++ core/resources/views/third-party/reset-password.blade.php
@@ -774,6 +774,8 @@ $bg = get_setting_value('login_bg') != '' ? image_url(get_setting_value('login_b
 @endphp
 
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 <script>
     $(function(){
