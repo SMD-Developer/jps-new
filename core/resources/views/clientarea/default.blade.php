f91ca7cd54eca08dfc81c4573da7a8fc4cfd0983
ashutosh8543 <ashutosh@smdwebtech.com>
2026-02-10 12:49:59 +0530

    default

--- core/resources/views/clientarea/default.blade.php
+++ core/resources/views/clientarea/default.blade.php
@@ -622,7 +622,7 @@ body {
             position: relative;
             bottom: 0;
             width: 100%;
-            top: 334px;
+            top: 40px;
             text-align: center;
             font-size: 12px;
             line-height: 1.4;
